2b889074660253e80c99aee2c410cf9ecf2419f9
Juan Basso <juan.basso@zumba.com>
2024-02-11 06:05:08 +0000

    Initial plugin version - Thanks ChatGPT

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -20,7 +20,6 @@ let debugMock: jest.SpyInstance
 let errorMock: jest.SpyInstance
 let getInputMock: jest.SpyInstance
 let setFailedMock: jest.SpyInstance
-let setOutputMock: jest.SpyInstance
 
 describe('action', () => {
   beforeEach(() => {
@@ -30,15 +29,16 @@ describe('action', () => {
     errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
     setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
   it('sets the time output', async () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation((name: string): string => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'github_token':
+          return 'gh-token'
+        case 'regex_patterns':
+          return 'pattern1,pattern2'
         default:
           return ''
       }
@@ -46,44 +46,6 @@ describe('action', () => {
 
     await main.run()
     expect(runMock).toHaveReturned()
-
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-    expect(errorMock).not.toHaveBeenCalled()
-  })
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
-    })
-
-    await main.run()
-    expect(runMock).toHaveReturned()
-
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
     expect(errorMock).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+
+enum DiffScope {
+  BOTH = 'both',
+  ADDED = 'added',
+  REMOVED = 'removed'
+}
 
 /**
  * The main function for the action.
@@ -7,18 +13,122 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token: string = core.getInput('github_token', { required: true })
+    const regexPatterns: string[] = core
+      .getInput('regex_patterns', { required: true })
+      .split(',')
+    const diffScope: DiffScope = (core.getInput('diff_scope') ||
+      'both') as DiffScope
+    const markChangesRequested: boolean =
+      core.getInput('mark_changes_requested') === 'true'
+    const matchFoundMessage: string = core.getInput('match_found_message')
+    const noMatchFoundMessage: string = core.getInput('no_match_found_message')
+    const changesRequestedMessage: string = core.getInput(
+      'changes_requested_message'
+    )
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = github.getOctokit(token)
+    const { context } = github
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug('Fetching pull request ...')
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.')
+      return
+    }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const pullRequestNumber = context.payload.pull_request.number
+    const owner = context.repo.owner
+    const repo = context.repo.repo
+
+    // Fetch PR diff
+    core.debug('Fetching pull request diff ...')
+    const { data: diffData } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+      mediaType: {
+        format: 'diff'
+      }
+    })
+
+    // Fetch existing comments on the PR
+    core.debug('Fetching existing comments ...')
+    const existingComments = await octokit.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number: pullRequestNumber
+    })
+
+    // Parse diff and search for regex matches
+    core.debug('Parsing diff and searching for matches ...')
+    const lines = (diffData as unknown as string).split('\n')
+    let foundMatches = false
+    let currentFile = ''
+    let currentLineNumber = 0
+
+    for (const line of lines) {
+      if (line.startsWith('+++ b/')) {
+        currentFile = line.substring('+++ b/'.length)
+        currentLineNumber = 0 // Reset line number for each new file
+      } else if (line.startsWith('+') || line.startsWith('-')) {
+        currentLineNumber++ // Count only added or removed lines
+      }
+
+      if (
+        ((diffScope === DiffScope.BOTH || diffScope === DiffScope.ADDED) &&
+          line.startsWith('+')) ||
+        ((diffScope === DiffScope.BOTH || diffScope === DiffScope.REMOVED) &&
+          line.startsWith('-'))
+      ) {
+        for (const pattern of regexPatterns) {
+          const regex = new RegExp(pattern)
+          if (regex.test(line)) {
+            // Before posting a new comment, check if it already exists
+            const isDuplicate = existingComments.data.some(
+              comment => comment.body === matchFoundMessage
+            )
+
+            if (!isDuplicate) {
+              core.debug(`Match found`)
+              foundMatches = true
+              const side = line.startsWith('+') ? 'RIGHT' : 'LEFT'
+              await octokit.rest.pulls.createReviewComment({
+                owner,
+                repo,
+                pull_number: pullRequestNumber,
+                body: matchFoundMessage,
+                commit_id: context.payload.pull_request.head.sha,
+                path: currentFile,
+                side: side,
+                line: currentLineNumber
+              })
+            } else {
+              core.debug(`Match found but already commented`)
+            }
+          }
+        }
+      }
+    }
+
+    if (!foundMatches) {
+      await octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: pullRequestNumber,
+        body: noMatchFoundMessage
+      })
+    }
+
+    // Optional: Mark PR as changes requested
+    if (markChangesRequested && foundMatches) {
+      await octokit.rest.pulls.createReview({
+        owner,
+        repo,
+        pull_number: pullRequestNumber,
+        event: 'REQUEST_CHANGES',
+        body: changesRequestedMessage
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
